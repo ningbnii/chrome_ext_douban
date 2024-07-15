0911414456552c6b600a37f2df6dd2be3a62bdd8
ningbnii <>
2024-07-15 16:10:15 +0800

    first

---
+++ src/contentScript/Content.vue
@@ -0,0 +1,26 @@
+<!-- src/content/Content.vue -->
+<template>
+  <!-- tailwindCss:一个按钮，浮动在屏幕右侧，屏幕中间位置 -->
+  <div class="fixed right-0 top-1/2 transform -translate-y-1/2">
+    <button @click="goToZlib" class="bg-blue-500 text-white px-4 py-2 rounded-full">去下载</button>
+  </div>
+</template>
+
+<script setup>
+let title = ''
+try {
+  // 获取.article-title的值
+  title = document.querySelector('.article-title').textContent.trim()
+} catch (error) {
+  try {
+    title = document.querySelector('.title-wrapper .title').textContent.trim()
+  } catch (error) {}
+}
+
+const goToZlib = () => {
+  // 打开新窗口
+  window.open(`https://zh.singlelogin.re/s/${title}?`)
+}
+</script>
+
+<style></style>
---
+++ src/popup/Popup.vue
@@ -0,0 +1,19 @@
+<script setup lang="js"></script>
+
+<template>
+  <div class="w-80 bg-gray-100 flex items-center justify-center min-h-screen">
+    <div class="bg-white p-8 rounded shadow-md max-w-lg text-center">
+      <h1 class="text-2xl font-bold mb-4">插件介绍</h1>
+      <p class="text-gray-700 text-xl text-justify">
+        这个插件可以让你在浏览豆瓣读书页面时，一键查询并下载对应书籍的Zlib资源
+      </p>
+      <!-- 温馨提示： 请确保下载的书籍仅用于个人学习和研究目的，尊重版权，支持正版。 -->
+      <p class="text-gray-700 p-4 text-justify">
+        <span class="text-red-500">温馨提示：</span>
+        请确保下载的书籍仅用于个人学习和研究目的，尊重版权，支持正版。
+      </p>
+    </div>
+  </div>
+</template>
+
+<style></style>
